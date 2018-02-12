c8fb2b3a5840fef7d689ed5cee8e3483a0dfd72c
JC Lewis <jclewis@justinewissimac.home>
2018-02-11 21:30:02 -0800

    created game logic functions

--- js/main.js
+++ js/main.js
@@ -1,25 +1,26 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
 
-cardsInPlay.push(cardOne);
+var checkForMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+			console.log("You found a match!");
+			//alert("You found a match!");
+		}
+		else {
+			console.log("Sorry, try again.");
+			//alert("Sorry, try again.");
+		}
+};
 
-console.log("User flipped " + cardOne);
+var flipCard = function(cardId){
+	var flippedCard = cards[cardId];
+	console.log("User flipped " + flippedCard);
+	cardsInPlay.push(flippedCard);
 
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2)
-{
-	if (cardsInPlay[0] === cardsInPlay[1])
-	{
-		alert("You found a match!");
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
 	}
-	else
-	{
-		alert("Sorry, try again.");
-	}
-}
+};
+
+flipCard(0);
+flipCard(2);
